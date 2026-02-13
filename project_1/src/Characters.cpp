9492ef59b9330517f3c9302441ed8ae3210ff954
WaylonErlandson <waylonerlandson@usf.edu>
2026-02-13 17:52:42 -0500

    Add Patrick character

--- project_1/src/Characters.cpp
+++ project_1/src/Characters.cpp
@@ -1,1 +1,43 @@
-#include "Character.hpp"
+#include "Characters.hpp"
+
+PatrickStar::PatrickStar(CNode<sf::Vector2f> *position) : position(position)
+{
+    star.setPointCount(5);
+    star.setPoint(0, sf::Vector2f(0.f, -50.f));
+    star.setPoint(1, sf::Vector2f(47.f, 15.f));
+    star.setPoint(2, sf::Vector2f(29.f, 50.f));
+    star.setPoint(3, sf::Vector2f(-29.f, 50.f));
+    star.setPoint(4, sf::Vector2f(-47.f, 15.f));
+    star.setFillColor(sf::Color(255, 192, 203));
+    star.setPosition(position->elem);
+
+    pants.setSize(sf::Vector2f(60.f, 25.f));
+    pants.setFillColor(sf::Color(0, 128, 0));
+    pants.setOrigin({30.f, 12.5f});
+
+    leftEye.setRadius(7.f);
+    rightEye.setRadius(7.f);
+    leftEye.setFillColor(sf::Color::White);
+    rightEye.setFillColor(sf::Color::White);
+    leftEye.setOrigin({7.f, 7.f});
+    rightEye.setOrigin({7.f, 7.f});
+}
+
+void PatrickStar::update(float deltaTime)
+{
+    position = position->next;
+    sf::Vector2f center = position->elem;
+    star.setPosition(center);
+
+    pants.setPosition({center.x, center.y + 25.f});
+    leftEye.setPosition({center.x - 15.f, center.y - 20.f});
+    rightEye.setPosition({center.x + 15.f, center.y - 20.f});
+}
+
+void PatrickStar::draw(sf::RenderWindow &window) const
+{
+    window.draw(star);
+    window.draw(pants);
+    window.draw(leftEye);
+    window.draw(rightEye);
+}
--- project_1/src/Characters.hpp
+++ project_1/src/Characters.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include "CircleList.hpp"
+#include <memory>
 
 class Character
 {
@@ -7,4 +9,18 @@ public:
     virtual ~Character() = default;
     virtual void update(float deltaTime) = 0;
     virtual void draw(sf::RenderWindow &window) const = 0;
+};
+
+class PatrickStar : public Character
+{
+public:
+    PatrickStar(CNode<sf::Vector2f> *position);
+    void update(float deltaTime) override;
+    void draw(sf::RenderWindow &window) const override;
+
+private:
+    sf::ConvexShape star;
+    sf::RectangleShape pants;
+    sf::CircleShape leftEye, rightEye;
+    CNode<sf::Vector2f> *position;
 };
