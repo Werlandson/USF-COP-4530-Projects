02a36dc348a5d92f8b715663ba853c24a0b7b49b
WaylonErlandson <waylonerlandson@usf.edu>
2026-02-14 21:13:32 -0500

    Add documentation

--- project_1/HotPotato.cpp
+++ project_1/HotPotato.cpp
@@ -1,9 +1,20 @@
+/*
+ * Name: Minh Duong, Waylon Erlandson, Long Nguyen
+ * Descriptions: Implements a simple hot potato game where a person is eliminated after holding the
+ * potato for a given time threshold. The last person standing wins. The program assumes that the first
+ * two inputs, number of players and name, are int and string, respectively and does not handle type
+ * errors. Additionally, the program assumes that the user is a fair sport and only presses enter when
+ * they are prompted to. If the user preemptively presses enter, the subsequent turn will take approximately
+ * zero time, and the user will unfairly win.
+ * */
+
 #include <thread>
 #include <chrono>
 #include <random>
 #include <iostream>
 using namespace std;
 
+// Generic linked list node
 template <typename T>
 class CNode
 {
@@ -12,6 +23,7 @@ public:
     CNode *next;
 };
 
+// Generic circularly linked list with add, remove, empty, advance, size, and getCursor abstract functionality
 template <typename T>
 class CircleList
 {
@@ -19,6 +31,7 @@ public:
     CircleList() : cursor(NULL), size(0) {}
     ~CircleList()
     {
+        // Removes nodes until the list is empty
         while (!empty())
             remove();
     }
@@ -38,6 +51,7 @@ public:
     {
         CNode<T> *v = new CNode<T>;
         v->elem = elem;
+        // If list is empty, cursor points to itself
         if (cursor == NULL)
         {
             cursor = v;
@@ -54,8 +68,9 @@ public:
     {
         CNode<T> *old = cursor->next;
 
+        // If there is one node in the list, cursor is set to NULL
         if (old == cursor)
-            cursor = NULL;
+            cursor = nullptr;
         else
             cursor->next = old->next;
 
@@ -73,8 +88,8 @@ private:
     int size;
 };
 
-// Stores a player name and time spent touching potato
-// Provides functions for updating time spent touching potato and accesing player name
+// Stores a player name, whether the player is the user, and time spent touching potato
+// Provides functions for updating time spent touching potato and accessing player name and boolean for whether the player is the user.
 class Player
 {
 private:
@@ -105,10 +120,12 @@ public:
     }
 };
 
+// Takes a circularly linked list of players and provides functionality for playing an entire Hot Potato game with the list of players.
 class Game
 {
 public:
     Game() : round(1) {}
+    // Returns a boolean for whether the game ended
     bool gameEnded()
     {
         if (players.getSize() == 1)
@@ -120,6 +137,7 @@ public:
             return false;
         }
     };
+    // Starts a hot potato game, calling playRound until gameEnded is true and printing contextual information
     void playGame()
     {
         // Clear leftover newline from the buffer
@@ -127,6 +145,7 @@ public:
 
         cout << "\nStarting Hot Potato Game: \nRemember, don't hold the hot potato for too long!\n\n";
 
+        // If the game has not ended, start another round.
         while (!gameEnded())
         {
             playRound();
@@ -134,20 +153,27 @@ public:
         }
 
         cout << endl;
+        // Remaining player is the champion.
         cout << players.getCursor()->elem.getName() << " is the Hot Potato champion!!!" << endl;
     }
+    /* Starts a Hot Potato round. The potato passes through each player until a player has held the potato for 3 seconds. For the user,
+     * the time taken to press enter is the time added to their timeTouchingPotato score. For non-users, the time is a random number
+     * between 0.15 and 1 seconds.
+     *  */
     void playRound()
     {
         // Initializes random number generator, range [0.05, 1], for the time a potato is held by the bot.
         random_device rd;
         mt19937 gen(rd());
         uniform_real_distribution<double> dist(0.15, 1.0);
+
         std::cout << "Round " << round << ": " << endl;
         std::this_thread::sleep_for(std::chrono::duration<double>(1));
         std::cout << endl
                   << "Start!" << endl
                   << endl;
 
+        // Infinite loop iterates through players until a break statement is met.
         while (true)
         {
             CNode<Player> *currPlayer = players.getCursor();
@@ -155,6 +181,8 @@ public:
             if (currPlayer->elem.getIsUser())
             {
                 cout << "Your turn! Press enter to pass the potato: " << endl;
+
+                // If currPlayer is user, timeTouchingPotato is determined by how long it takes for the user to press enter.
                 auto start = std::chrono::steady_clock::now();
                 std::cin.get();
                 auto end = std::chrono::steady_clock::now();
@@ -164,6 +192,7 @@ public:
             }
             else
             {
+                // timeTouchingPotato is a random number in range [0.15, 1]
                 double timeTouchingPotato = dist(gen);
                 std::this_thread::sleep_for(std::chrono::duration<double>(timeTouchingPotato));
                 currPlayer->elem.addTimeTouchingPotato(timeTouchingPotato);
@@ -174,12 +203,14 @@ public:
                 std::cout
                     << currPlayer->elem.getName() << " has been eliminated." << endl
                     << endl;
+                // Moves cursor to the node before the eliminated node for removal purposes while giving players additional time
                 for (int j = 0; j < players.getSize() - 1; j++)
                 {
                     players.advance();
                     // Gives the player additional time to touch potato in next round.
                     players.getCursor()->elem.addTimeTouchingPotato(-1.0);
                 }
+                // Removes eliminated player from players list
                 players.remove();
                 break;
             }
