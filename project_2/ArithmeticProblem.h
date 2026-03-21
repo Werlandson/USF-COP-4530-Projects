5fda036f85e1628273a68d5b25a372147489055e
longga1111 <160759967+longga1111@users.noreply.github.com>
2026-03-21 00:03:58 -0400

    Update ArithmeticProblem.h

--- project_2/ArithmeticProblem.h
+++ project_2/ArithmeticProblem.h
@@ -1,17 +1,21 @@
 /*
- * Name: Waylon Erlandson
+ * Name: Waylon Erlandson, Minh Duong, Duc Long Nguyen
  * Description: Header file for ArithmeticProblem class. Each arithmetic problem has two operands, an operator (+, -, *, or /),
  * and a corresponding answer.
  */
 #pragma once
 #include <vector>
 
+using namespace std;
+
 // A class for storing arithmetic problems and answers and copying those arithmetic problems
 class ArithmeticProblem
 {
 public:
+    // Constructor to initialize an arithmetic problem.
     ArithmeticProblem(int operand1, int operand2, char operator1, int answer)
         : operand1(operand1), operand2(operand2), operator1(operator1), answer(answer) {}
+    // Copy constructor to duplicate an existing arithmetic problem.
     ArithmeticProblem(const ArithmeticProblem &problem)
         : operand1(problem.operand1), operand2(problem.operand2), operator1(problem.operator1), answer(problem.answer) {}
 
@@ -21,4 +25,4 @@ public:
     int answer;
 };
 
-std::vector<ArithmeticProblem> createArithmeticProblems();
+vector<ArithmeticProblem> createArithmeticProblems();
