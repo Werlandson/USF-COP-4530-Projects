a00c1de5b101d14d5efd11018d146d490107272c
longga1111 <160759967+longga1111@users.noreply.github.com>
2026-03-21 00:11:17 -0400

    Update Restaurant.h

--- project_2/Restaurant.h
+++ project_2/Restaurant.h
@@ -1,5 +1,5 @@
 /*
- * Name: Waylon Erlandson
+ * Name: Waylon Erlandson, Duong Minh, Duc Long Nguyen
  * Description: Header file for Restaurant class. Restaurant inherits the shared Company simulation
  * behavior and differentiates itself through arithmetic-problem serving tasks.
  * */
@@ -10,14 +10,19 @@
 #include "ArithmeticProblem.h"
 #include "Company.h"
 
+using namespace std;
+
 class Restaurant : public Company
 {
 public:
+    // Constructor for Restaurant.
     Restaurant();
 
 private:
+    // Prints the welcome message.
     void printWelcome() override;
-    void performServiceTask(const Group &group, std::mt19937 &gen) override;
+    // Performs the service task by presenting arithmetic problems to solve.
+    void performServiceTask(const Group &group, mt19937 &gen) override;
 
-    std::vector<ArithmeticProblem> arithmeticProblems;
+    vector<ArithmeticProblem> arithmeticProblems;
 };
